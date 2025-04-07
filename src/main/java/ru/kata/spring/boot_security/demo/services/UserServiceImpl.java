b9e490615df7bb8cf114d90ef04970ff588d4476
Tomas-Mart <amina.tomas.mart@icloud.com>
2025-04-07 19:50:03 +0400

    Test

--- src/main/java/ru/kata/spring/boot_security/demo/services/UserServiceImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/services/UserServiceImpl.java
@@ -1,7 +1,5 @@
 package ru.kata.spring.boot_security.demo.services;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
