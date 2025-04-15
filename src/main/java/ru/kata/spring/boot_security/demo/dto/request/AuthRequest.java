d7fe5be38ffe6a6fff086c4b901014f0500c987a
Tomas-Mart <amina.tomas.mart@icloud.com>
2025-04-15 11:51:53 +0400

    Доработка

--- src/main/java/ru/kata/spring/boot_security/demo/dto/request/AuthRequest.java
+++
@@ -1,3 +0,0 @@
-package ru.kata.spring.boot_security.demo.dto.request;
-
-public record AuthRequest(String username, String password) {}
--- src/main/java/ru/kata/spring/boot_security/demo/dto/response/AuthResponse.java
+++
@@ -1,3 +0,0 @@
-package ru.kata.spring.boot_security.demo.dto.response;
-
-public record AuthResponse(String token) {}
